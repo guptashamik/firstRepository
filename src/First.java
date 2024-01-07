70db3a2e402946d810d8501bfe9b6e41914a6060
guptashamik <155887415+guptashamik@users.noreply.github.com>
2024-01-07 19:22:17 +0530

    Update First.java

--- src/First.java
+++ src/First.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.v110.input.Input;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver1;
 
 public class First {
 
